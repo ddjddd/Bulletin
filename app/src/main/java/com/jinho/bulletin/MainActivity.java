fcdc9443d526dbd20be63b19ede970a462d292d9
jinho.choi <ddjddd@naver.com>
2017-11-23 17:15:43 +0900

    Add firebase classes

--- app/src/main/java/com/jinho/bulletin/MainActivity.java
+++ app/src/main/java/com/jinho/bulletin/MainActivity.java
@@ -3,11 +3,41 @@ package com.jinho.bulletin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+
 public class MainActivity extends AppCompatActivity {
+    private Button user_secondhand;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user_secondhand = (Button) findViewById(R.id.user_secondhand);
+
+        user_secondhand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SecondhandBulletinActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
