52cec989a8a0841c34f340d31e2bd93bb019babe
WooJS <jswoo0921@naver.com>
2017-12-01 21:16:45 +0900

    update

--- app/src/main/java/com/jinho/bulletin/ProfileActivity.java
+++ app/src/main/java/com/jinho/bulletin/ProfileActivity.java
@@ -8,12 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -23,20 +29,38 @@ public class ProfileActivity extends AppCompatActivity {
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
+
+    private Button Misson, Next, Secondhand;
+    private EditText Search;
+
+    private List<String> list;
+    private ListView listView;
+    private EditText editSearch;
+    private ArrayList<String> arrayList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
-        profile_next = (Button) findViewById(R.id.pf_next);
+        //editSearch = (EditText) findViewById(R.id.editSearch);
+        //listView = (ListView) findViewById(R.id.listview);
 
+        // 이하 xx
+        profile_next = (Button) findViewById(R.id.pf_next);
         profile_next.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(getApplicationContext(), SecondhandBulletinActivity.class);
                 startActivity(intent);
             }
+
         });
+        // xx
+
+
     }
+
 }
