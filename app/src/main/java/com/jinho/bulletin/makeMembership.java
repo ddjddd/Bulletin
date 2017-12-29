2036474251bfaeed7b0b63cfcf8d95eab89d2535
HanJungWoo <ajtwlswjddnv@naver.com>
2017-12-29 19:36:37 +0900

    hanjungwoo

---
+++ app/src/main/java/com/jinho/bulletin/makeMembership.java
@@ -0,0 +1,76 @@
+package com.jinho.bulletin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class makeMembership extends AppCompatActivity {
+
+    private EditText UserID, UserPW;
+    private Button registeer_membership;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_membership);
+
+        UserID = (EditText) findViewById(R.id.userID);
+        UserPW = (EditText) findViewById(R.id.userPW);
+        registeer_membership = (Button) findViewById(R.id.button_membership);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        registeer_membership.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(makeMembership.this, "Authentication success.",
+                        Toast.LENGTH_SHORT).show();
+                createUser(UserID.getText().toString(),UserPW.getText().toString());
+            }
+        });
+
+
+    }
+
+    private void createUser(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(makeMembership.this, "Authentication success.",
+                                    Toast.LENGTH_SHORT).show();
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            // 로그인 페이지로이동
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(makeMembership.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+}
